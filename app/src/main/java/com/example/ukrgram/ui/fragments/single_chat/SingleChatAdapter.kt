5fe399e6eb0911701280f6d5bc5471ba1d7af1d2
ivansyvak <vanyafc78@gmail.com>
2023-01-26 17:43:24 +0200

    подгрузка сообщений по 10

--- app/src/main/java/com/example/ukrgram/ui/fragments/single_chat/SingleChatAdapter.kt
+++ app/src/main/java/com/example/ukrgram/ui/fragments/single_chat/SingleChatAdapter.kt
@@ -53,16 +53,14 @@ class SingleChatAdapter : RecyclerView.Adapter<SingleChatAdapter.SingleChatHolde
         }
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun setList(list: List<CommonModel>) {
-
-//        notifyDataSetChanged()
-    }
 
     fun addItem(item: CommonModel) {
         val newList = mutableListOf<CommonModel>()
         newList.addAll(mlistMessagesCache)
-        newList.add(item)
+
+        if (!newList.contains(item)) newList.add(item)
+
+        newList.sortBy { it.timeStamp.toString() }
         mDiffResult = DiffUtil.calculateDiff(DiffUtilCallback(mlistMessagesCache, newList))
         mDiffResult.dispatchUpdatesTo(this)
         mlistMessagesCache = newList
--- app/src/main/java/com/example/ukrgram/ui/fragments/single_chat/SingleChatFragment.kt
+++ app/src/main/java/com/example/ukrgram/ui/fragments/single_chat/SingleChatFragment.kt
@@ -1,6 +1,7 @@
 package com.example.ukrgram.ui.fragments.single_chat
 
 import android.view.View
+import android.widget.AbsListView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ukrgram.R
 import com.example.ukrgram.models.CommonModel
@@ -27,8 +28,11 @@ class SingleChatFragment(private val contact: CommonModel) :
     private lateinit var mRefMessages: DatabaseReference
     private lateinit var mAdapter: SingleChatAdapter
     private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mMessagesListener: ChildEventListener
-    private var mListMessages = mutableListOf<CommonModel>()
+    private lateinit var mMessagesListener: AppChildEventListener
+    private var mCountMessages = 10
+    private var mIsScrolling = false
+    private var mSmoothScrollToPosition = true
+    private var mListListener = mutableListOf<AppChildEventListener>()
 
     override fun onResume() {
         super.onResume()
@@ -47,11 +51,39 @@ class SingleChatFragment(private val contact: CommonModel) :
 
         mMessagesListener = AppChildEventListener {
             mAdapter.addItem(it.getCommonModel())
-            mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
+            if (mSmoothScrollToPosition) {
+                mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
+            }
         }
 
 
-        mRefMessages.addChildEventListener(mMessagesListener)
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
+        mListListener.add(mMessagesListener)
+
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    mIsScrolling = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (mIsScrolling && dy < 0) {
+                    updateData()
+                }
+            }
+
+        })
+    }
+
+    private fun updateData() {
+        mSmoothScrollToPosition = false
+        mIsScrolling = false
+        mCountMessages += 10
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
+        mListListener.add(mMessagesListener)
     }
 
     private fun initToolbar() {
@@ -65,6 +97,7 @@ class SingleChatFragment(private val contact: CommonModel) :
         mRefUser.addValueEventListener(mListenerInfoToolbar)
 
         chat_btn_send_message.setOnClickListener {
+            mSmoothScrollToPosition = true
             val message = chat_input_message.text.toString()
             if (message.isEmpty()) {
                 showToast("Введите сообщение")
@@ -89,6 +122,9 @@ class SingleChatFragment(private val contact: CommonModel) :
         super.onPause()
         APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.GONE
         mRefUser.removeEventListener(mListenerInfoToolbar)
-        mRefMessages.removeEventListener(mMessagesListener)
+        mListListener.forEach {
+            mRefMessages.removeEventListener(it)
+
+        }
     }
 }

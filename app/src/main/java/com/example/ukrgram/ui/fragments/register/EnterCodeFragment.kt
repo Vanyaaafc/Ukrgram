88ae32cf38c441c81e8cab6894f7d683b99ef926
ivansyvak <vanyafc78@gmail.com>
2023-02-11 19:14:04 +0200

    voice message. part 1

--- app/src/main/java/com/example/ukrgram/ui/fragments/register/EnterCodeFragment.kt
+++ app/src/main/java/com/example/ukrgram/ui/fragments/register/EnterCodeFragment.kt
@@ -7,6 +7,7 @@ import com.example.ukrgram.ui.fragments.BaseFragment
 import com.example.ukrgram.utilits.*
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.android.synthetic.main.fragment_enter_code.*
 
 
 class EnterCodeFragment(val phoneNumber: String, val id: String) :
@@ -16,19 +17,18 @@ class EnterCodeFragment(val phoneNumber: String, val id: String) :
     override fun onStart() {
         super.onStart()
         APP_ACTIVITY.title = phoneNumber
-        mRootView.findViewById<EditText>(R.id.register_input_code)
-            .addTextChangedListener(AppTextWatcher {
-                val string =
-                    mRootView.findViewById<EditText>(R.id.register_input_code).text.toString()
-                if (string.length == 6) {
-                    enterCode()
-                }
-            })
+        register_input_code.addTextChangedListener(AppTextWatcher {
+            val string =
+                mRootView.findViewById<EditText>(R.id.register_input_code).text.toString()
+            if (string.length == 6) {
+                enterCode()
+            }
+        })
     }
 
     private fun enterCode() {
         val code: String =
-            mRootView.findViewById<EditText>(R.id.register_input_code).text.toString()
+            register_input_code.text.toString()
         val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(id, code)
         AUTH.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {
@@ -36,19 +36,27 @@ class EnterCodeFragment(val phoneNumber: String, val id: String) :
                 val dateMap = mutableMapOf<String, Any>()
                 dateMap[CHILD_ID] = uid
                 dateMap[CHILD_PHONE] = phoneNumber
-                dateMap[CHILD_USERNAME] = uid
 
-                REF_DATABASE_ROOT.child(NODE_PHONES).child(phoneNumber).setValue(uid)
-                    .addOnFailureListener { showToast(it.message.toString()) }
-                    .addOnSuccessListener {
-                        REF_DATABASE_ROOT.child(NODE_USERS).child(uid).updateChildren(dateMap)
-                            .addOnSuccessListener {
-                                showToast("Добро пожаловать")
-                                restartActivity()
-                                hideKeyboard()
-                            }
+                REF_DATABASE_ROOT.child(NODE_USERS).child(uid)
+                    .addListenerForSingleValueEvent(AppValueEventListener {
+
+                        if (!it.hasChild(CHILD_USERNAME)) {
+                            dateMap[CHILD_USERNAME] = uid
+                        }
+
+                        REF_DATABASE_ROOT.child(NODE_PHONES).child(phoneNumber).setValue(uid)
                             .addOnFailureListener { showToast(it.message.toString()) }
-                    }
+                            .addOnSuccessListener {
+                                REF_DATABASE_ROOT.child(NODE_USERS).child(uid)
+                                    .updateChildren(dateMap)
+                                    .addOnSuccessListener {
+                                        showToast("Добро пожаловать")
+                                        restartActivity()
+                                        hideKeyboard()
+                                    }
+                                    .addOnFailureListener { showToast(it.message.toString()) }
+                            }
+                    })
 
             } else {
                 showToast(task.exception?.message.toString())

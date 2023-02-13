260aa9595583453e01428e0b2ba9c9fc5c015bc7
ivansyvak <vanyafc78@gmail.com>
2023-02-13 18:10:33 +0200

    пробник

--- app/src/main/java/com/example/ukrgram/database/databaseFuns.kt
+++ app/src/main/java/com/example/ukrgram/database/databaseFuns.kt
@@ -16,7 +16,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.util.ArrayList
 
-//a
+//aф
 fun initFirebase() {
     AUTH = FirebaseAuth.getInstance()
     REF_DATABASE_ROOT = FirebaseDatabase.getInstance().reference
